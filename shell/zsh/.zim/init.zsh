63aa693e2791f279cdd74c886989aeacbe5d8807
carlosdavila91 <carlos.davila.luelmo@gmail.com>
2026-02-01 12:55:26 +0100

    Update zim

--- shell/zsh/.zim/init.zsh
+++ shell/zsh/.zim/init.zsh
@@ -1,4 +1,4 @@
-# FILE AUTOMATICALLY GENERATED FROM /home/luecarda/.zimrc
+# FILE AUTOMATICALLY GENERATED FROM /home/carlosatm/.zimrc
 # EDIT THE SOURCE FILE AND THEN RUN zimfw build. DO NOT DIRECTLY EDIT THIS FILE!
 
 if [[ -e ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]] zimfw() { source "${HOME}/.dotfiles/shell/zsh/.zim/zimfw.zsh" "${@}" }
